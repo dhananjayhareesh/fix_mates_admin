1c906c685530b5430e56c1e3336eea84356b2260
Dhananjay Hareesh <dhananjay.hareesh@gmail.com>
2024-08-13 17:36:25 +0530

    block unbloock done

--- lib/view/sevice_partner.dart
+++ lib/view/sevice_partner.dart
@@ -26,167 +26,112 @@ class SevricePartner extends StatelessWidget {
   }
 
   Widget _buildMobileLayout() {
-    return StreamBuilder<QuerySnapshot>(
-      stream: FirebaseFirestore.instance.collection('workers').snapshots(),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return Center(child: CircularProgressIndicator());
-        }
-
-        if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
-          return Center(
-              child: Text('No workers found.',
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)));
-        }
-
-        var workers = snapshot.data!.docs;
-
-        return ListView.builder(
-          padding: EdgeInsets.all(8),
-          itemCount: workers.length,
-          itemBuilder: (context, index) {
-            var worker = workers[index].data() as Map<String, dynamic>;
-            String id = workers[index].id; // Get the ID of the document
-            return Card(
-              margin: EdgeInsets.symmetric(vertical: 8),
-              elevation: 4,
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(12)),
-              child: ListTile(
-                contentPadding: EdgeInsets.all(16),
-                leading: worker['photoUrl'] != null
-                    ? ClipRRect(
-                        borderRadius: BorderRadius.circular(8),
-                        child: Image.network(worker['photoUrl'],
-                            width: 60, height: 60, fit: BoxFit.cover),
-                      )
-                    : Container(
-                        width: 60,
-                        height: 60,
-                        color: Colors.grey,
-                        child: Icon(Icons.person, color: Colors.white)),
-                title: Text(worker['userName'],
-                    style: TextStyle(fontWeight: FontWeight.bold)),
-                subtitle: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(worker['userEmail']),
-                    SizedBox(height: 4),
-                    Text('Category: ${worker['category']}',
-                        style: TextStyle(color: Colors.grey[600])),
-                    SizedBox(height: 4),
-                    Text('ID: $id', style: TextStyle(color: Colors.grey[600])),
-                    SizedBox(height: 4),
-                    worker['idCardUrl'] != null
-                        ? GestureDetector(
-                            onTap: () =>
-                                _showImage(context, worker['idCardUrl']),
-                            child: Text('ID Card Available',
-                                style: TextStyle(
-                                    color: Colors.blue,
-                                    fontWeight: FontWeight.bold)),
-                          )
-                        : Text('No ID Card Available',
-                            style: TextStyle(color: Colors.grey[600])),
-                  ],
-                ),
-              ),
-            );
-          },
-        );
-      },
-    );
+    return _buildWorkersList();
   }
 
   Widget _buildTabletLayout() {
+    return _buildWorkersList();
+  }
+
+  Widget _buildDesktopLayout() {
+    return _buildWorkersList();
+  }
+
+  Widget _buildWorkersList() {
     return StreamBuilder<QuerySnapshot>(
       stream: FirebaseFirestore.instance.collection('workers').snapshots(),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return Center(child: CircularProgressIndicator());
-        }
+      builder: (context, workersSnapshot) {
+        return StreamBuilder<QuerySnapshot>(
+          stream: FirebaseFirestore.instance
+              .collection('blockedWorkers')
+              .snapshots(),
+          builder: (context, blockedWorkersSnapshot) {
+            if (workersSnapshot.connectionState == ConnectionState.waiting ||
+                blockedWorkersSnapshot.connectionState ==
+                    ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+            }
 
-        if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
-          return Center(
-              child: Text('No workers found.',
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)));
-        }
+            var workers = workersSnapshot.data?.docs ?? [];
+            var blockedWorkers = blockedWorkersSnapshot.data?.docs ?? [];
 
-        var workers = snapshot.data!.docs;
+            if (workers.isEmpty && blockedWorkers.isEmpty) {
+              return Center(
+                  child: Text('No workers found.',
+                      style: TextStyle(
+                          fontSize: 18, fontWeight: FontWeight.bold)));
+            }
 
-        return GridView.builder(
-          padding: EdgeInsets.all(16),
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            crossAxisSpacing: 16,
-            mainAxisSpacing: 16,
-            childAspectRatio: 0.8,
-          ),
-          itemCount: workers.length,
-          itemBuilder: (context, index) {
-            var worker = workers[index].data() as Map<String, dynamic>;
-            String id = workers[index].id; // Get the ID of the document
-            return Card(
-              elevation: 4,
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(12)),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Expanded(
-                    child: worker['photoUrl'] != null
+            var allWorkers = workers + blockedWorkers;
+
+            return ListView.builder(
+              padding: EdgeInsets.all(8),
+              itemCount: allWorkers.length,
+              itemBuilder: (context, index) {
+                var worker = allWorkers[index].data() as Map<String, dynamic>;
+                String id = allWorkers[index].id; // Get the ID of the document
+                bool isBlocked = blockedWorkers.any((doc) => doc.id == id);
+
+                return Card(
+                  margin: EdgeInsets.symmetric(vertical: 8),
+                  elevation: 4,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12)),
+                  child: ListTile(
+                    contentPadding: EdgeInsets.all(16),
+                    leading: worker['photoUrl'] != null
                         ? ClipRRect(
-                            borderRadius:
-                                BorderRadius.vertical(top: Radius.circular(12)),
+                            borderRadius: BorderRadius.circular(8),
                             child: Image.network(worker['photoUrl'],
-                                width: double.infinity, fit: BoxFit.cover),
+                                width: 60, height: 60, fit: BoxFit.cover),
                           )
                         : Container(
-                            width: double.infinity,
+                            width: 60,
+                            height: 60,
                             color: Colors.grey,
-                            child: Center(
-                                child: Icon(Icons.person,
-                                    color: Colors.white, size: 50)),
-                          ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(worker['userName'],
+                            child: Icon(Icons.person, color: Colors.white)),
+                    title: Text(worker['userName'],
                         style: TextStyle(fontWeight: FontWeight.bold)),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                    child: Text(worker['userEmail']),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 8.0, vertical: 4.0),
-                    child: Text('Category: ${worker['category']}',
-                        style: TextStyle(color: Colors.grey[600])),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 8.0, vertical: 4.0),
-                    child: Text('ID: $id',
-                        style: TextStyle(color: Colors.grey[600])),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 8.0, vertical: 4.0),
-                    child: worker['idCardUrl'] != null
-                        ? GestureDetector(
-                            onTap: () =>
-                                _showImage(context, worker['idCardUrl']),
-                            child: Text('ID Card Available',
-                                style: TextStyle(
-                                    color: Colors.blue,
-                                    fontWeight: FontWeight.bold)),
-                          )
-                        : Text('No ID Card Available',
+                    subtitle: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(worker['userEmail']),
+                        SizedBox(height: 4),
+                        Text('Category: ${worker['category']}',
                             style: TextStyle(color: Colors.grey[600])),
+                        SizedBox(height: 4),
+                        Text('ID: $id',
+                            style: TextStyle(color: Colors.grey[600])),
+                        SizedBox(height: 4),
+                        worker['idCardUrl'] != null
+                            ? GestureDetector(
+                                onTap: () =>
+                                    _showImage(context, worker['idCardUrl']),
+                                child: Text('ID Card Available',
+                                    style: TextStyle(
+                                        color: Colors.blue,
+                                        fontWeight: FontWeight.bold)),
+                              )
+                            : Text('No ID Card Available',
+                                style: TextStyle(color: Colors.grey[600])),
+                      ],
+                    ),
+                    trailing: IconButton(
+                      icon: Icon(
+                        isBlocked ? Icons.check_circle : Icons.block,
+                        color: isBlocked ? Colors.green : Colors.red,
+                      ),
+                      onPressed: () {
+                        if (isBlocked) {
+                          _unblockWorker(id, worker);
+                        } else {
+                          _blockWorker(id, worker);
+                        }
+                      },
+                    ),
                   ),
-                ],
-              ),
+                );
+              },
             );
           },
         );
@@ -194,84 +139,24 @@ class SevricePartner extends StatelessWidget {
     );
   }
 
-  Widget _buildDesktopLayout() {
-    return StreamBuilder<QuerySnapshot>(
-      stream: FirebaseFirestore.instance.collection('workers').snapshots(),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return Center(child: CircularProgressIndicator());
-        }
+  void _blockWorker(String id, Map<String, dynamic> workerData) {
+    FirebaseFirestore.instance
+        .collection('blockedWorkers')
+        .doc(id)
+        .set(workerData)
+        .then((_) {
+      FirebaseFirestore.instance.collection('workers').doc(id).delete();
+    });
+  }
 
-        if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
-          return Center(
-              child: Text('No workers found.',
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)));
-        }
-
-        var workers = snapshot.data!.docs;
-
-        return Column(
-          children: [
-            Expanded(
-              child: ListView.builder(
-                padding: EdgeInsets.all(16),
-                itemCount: workers.length,
-                itemBuilder: (context, index) {
-                  var worker = workers[index].data() as Map<String, dynamic>;
-                  String id = workers[index].id; // Get the ID of the document
-                  return Card(
-                    margin: EdgeInsets.symmetric(vertical: 8),
-                    elevation: 4,
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(12)),
-                    child: ListTile(
-                      contentPadding: EdgeInsets.all(16),
-                      leading: worker['photoUrl'] != null
-                          ? ClipRRect(
-                              borderRadius: BorderRadius.circular(8),
-                              child: Image.network(worker['photoUrl'],
-                                  width: 80, height: 80, fit: BoxFit.cover),
-                            )
-                          : Container(
-                              width: 80,
-                              height: 80,
-                              color: Colors.grey,
-                              child: Icon(Icons.person, color: Colors.white)),
-                      title: Text(worker['userName'],
-                          style: TextStyle(fontWeight: FontWeight.bold)),
-                      subtitle: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(worker['userEmail']),
-                          SizedBox(height: 4),
-                          Text('Category: ${worker['category']}',
-                              style: TextStyle(color: Colors.grey[600])),
-                          SizedBox(height: 4),
-                          Text('ID: $id',
-                              style: TextStyle(color: Colors.grey[600])),
-                          SizedBox(height: 4),
-                          worker['idCardUrl'] != null
-                              ? GestureDetector(
-                                  onTap: () =>
-                                      _showImage(context, worker['idCardUrl']),
-                                  child: Text('ID Card Available',
-                                      style: TextStyle(
-                                          color: Colors.blue,
-                                          fontWeight: FontWeight.bold)),
-                                )
-                              : Text('No ID Card Available',
-                                  style: TextStyle(color: Colors.grey[600])),
-                        ],
-                      ),
-                    ),
-                  );
-                },
-              ),
-            ),
-          ],
-        );
-      },
-    );
+  void _unblockWorker(String id, Map<String, dynamic> workerData) {
+    FirebaseFirestore.instance
+        .collection('workers')
+        .doc(id)
+        .set(workerData)
+        .then((_) {
+      FirebaseFirestore.instance.collection('blockedWorkers').doc(id).delete();
+    });
   }
 
   void _showImage(BuildContext context, String imageUrl) {
